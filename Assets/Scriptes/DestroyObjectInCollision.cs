b299bda4f53c4bace3e5b78bff3cad1f60f90fb4
Dário Santos <dariovfsantos@gmail.com>
2019-01-26 15:50:25 +0000

    Don't know

---
+++ Assets/Scriptes/DestroyObjectInCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DestroyObjectInCollision : MonoBehaviour
+{
+    [SerializeField] private string target = "Player";
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(target))
+            Destroy(gameObject);
+    }
+}
